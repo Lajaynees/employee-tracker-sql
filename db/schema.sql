e621cab4b87202d6c8149ef256730637765c9414
Lajaynees <lscingram@yahoo.com>
2021-09-22 23:29:02 -0400

    update schema and sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,32 @@
-DROP DATABASE IF EXISTS books_db;
-CREATE DATABASE books_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE books_db;
+USE employee_db;
 
-CREATE TABLE favorite_books (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(30) NOT NULL
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id iNT NOT NULL,
+    manager_id VARCHAR(30) NULL,
+    PRIMARY KEY(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,21 @@
-INSERT INTO favorite_books (book_name)
-VALUES ("The Great Gatsby"),
-       ("Huckleberry Finn"),
-       ("100 Years of Solitude"),
-       ("Things Fall Apart"),
-       ("Crime and Punishment"),
-       ("Moby Dick"),
-       ("Decameron");
-       
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES
+    ('Management'),
+    ('Co_Management'),
+    ('Sales'),
+    ('Pharmacy');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Store Manager', 200000, 1),
+    ('Assistant Manager', 70000, 1),
+    ('Team Lead', 50000, 2),
+    ('Inventory Specialist', 40000, 2),
+    ('Photo Specialist', 30000, 3),
+    ('Pharmacy Technician', 40000, 4),
+    ('Pharmacist', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES
